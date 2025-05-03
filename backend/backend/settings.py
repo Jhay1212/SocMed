3a3be85a39af9c9ae6adb1e7fe0b84ac85116297
Jhay12 <jhayjane78@gmail.com>
2025-05-03 12:35:24 +0800

    fix user issue

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '{{ SECRET_KEY }}' 
-
+SECRET_KEY = {'SECRET_KEY'}
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -48,6 +47,7 @@ INSTALLED_APPS = [
 
     # third-party
     'rest_framework',
+    'djoser',
     'corsheaders',
 ]
 
@@ -135,3 +135,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
